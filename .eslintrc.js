4dcdb025c696b92421911463c0a08748a2c2cdf0
xerocross <nonya@business.com>
2019-01-18 11:50:28 -0500

    remove Vue stuff

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,23 +9,15 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:vue/recommended"
     ],
     rules: {
         "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
         "indent": ["error", 4],
-        "vue/html-indent": ["error", 4, {
-            "attribute": 1,
-            "closeBracket": 0,
-            "alignAttributesVertically": true,
-            "ignores": []
-        }],
         "key-spacing": ["error", {
             "beforeColon": true,
             "afterColon": true,
             "mode": "strict"
-        }],
-        "vue/html-self-closing" : [0]
+        }]
     },
     globals: {
         "NODE_ENV" : false
--- webpack.config.js
+++ webpack.config.js
@@ -12,36 +12,9 @@ module.exports = [{
         filename : '[name].js',
     },
     externals : {
-        vue : "Vue" // remove this line if you want to bundle vue with the script
     },
     module : {
         rules : [
-            {
-                test : /\.vue$/,
-                exclude : /node_modules/,
-                loader : 'vue-loader'
-            },
-            {
-                test : /\.js$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : 'babel-loader'
-                }
-            },
-            {
-                test : /\.scss$/,
-                use : [
-                    {
-                        loader : 'style-loader' // creates style nodes from JS strings
-                    },
-                    {
-                        loader : 'css-loader' // translates CSS into CommonJS
-                    },
-                    {
-                        loader : 'sass-loader' // compiles Sass to CSS
-                    }
-                ]
-            }
         ]
     },
     optimization : {
@@ -59,62 +32,4 @@ module.exports = [{
         port : 9000,
         watchContentBase : true
     }
-},
-{
-    entry : {
-        'demoindex' : './src/demoindex.js'
-    },
-    output : {
-        path : path.resolve(__dirname, './dist'),
-        publicPath : '/dist',
-        filename : '[name].js',
-    },
-    externals : {
-    },
-    module : {
-        rules : [
-            {
-                test : /\.vue$/,
-                exclude : /node_modules/,
-                loader : 'vue-loader'
-            },
-            {
-                test : /\.js$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : 'babel-loader'
-                }
-            },
-            {
-                test : /\.scss$/,
-                use : [
-                    {
-                        loader : 'style-loader' // creates style nodes from JS strings
-                    },
-                    {
-                        loader : 'css-loader' // translates CSS into CommonJS
-                    },
-                    {
-                        loader : 'sass-loader' // compiles Sass to CSS
-                    }
-                ]
-            }
-        ]
-    },
-    optimization : {
-        minimize : false
-    },
-    plugins : [
-        new webpack.DefinePlugin({
-            'NODE_ENV' : JSON.stringify(process.env.NODE_ENV)
-        }),
-        new VueLoaderPlugin()
-    ],
-    devServer : {
-        contentBase : "./public",
-        compress : true,
-        port : 9000,
-        watchContentBase : true
-    }
-}
-]
+}]
